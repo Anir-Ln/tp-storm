fc6f0ea5a32cd34dca4eb8eabb0c1dcdd203a454
Anir-Ln <anirlahyane@gmail.com>
2023-12-02 17:25:51 +0100

    Create bolt for turtoise

---
+++ src/main/java/stormTP/operator/MyTortoiseBolt.java
@@ -0,0 +1,27 @@
+package stormTP.operator;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.Map;
+
+public class MyTortoiseBolt extends BaseRichBolt {
+    private OutputCollector collector;
+    @Override
+    public void prepare(Map<String, Object> topoConf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
+}
